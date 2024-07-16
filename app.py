c7cd7fd935a95b1eae81f44cc9fe6a6c0985169f
Keiran <127797224+GitHubWestie@users.noreply.github.com>
2024-07-16 14:59:25 +0100

    Added landing.html template and route and POST method for sign up

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (
     Flask, render_template, redirect,
-    request, session, url_for)
+    request, session, url_for, flash)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -18,12 +18,40 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+@app.route("/")
+@app.route("/landing")
+def landing():
+    return render_template("landing.html")
+
+
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
-    return render_template("sign_up.html")
+    if request.method == "POST":
+        user_exists = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()}
+        )
+
+        if user_exists:
+            flash("User already exists")
+            return redirect(url_for("sign_up"))
+        
+        new_user = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(new_user)
+
+        session["user"] = request.form.get("username").lower()
+        flash("User created successfully")
+
+    return render_template("sign-up.html")
 
 
-@app.route("/")
+@app.route("/sign_in")
+def sign_in():
+    return render_template("sign-in.html")
+
+
 @app.route("/my_bike_shed")
 def my_bike_shed():
     my_bike_shed = mongo.db.my_bike_shed.find()
